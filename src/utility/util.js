a7c2bb3f6ef4d5957f11f9e223e6f99f7969a3ca
Jaydeep <jaydeep0216@gmail.com>
2023-09-12 12:49:06 +0530

    Formatt Code

--- src/utility/util.js
+++ src/utility/util.js
@@ -1,50 +1,50 @@
 const nodemailer = require("nodemailer");
 const Appconfig = require("../config/config.json");
 
-function sendMail(from, to , subject, text) {
-    const transporter = nodemailer.createTransport({
-        service: Appconfig.SERVICE,
-        auth:{
-            user:Appconfig.email,
-            pass:Appconfig.password,
-        },
-    });
+function sendMail(from, to, subject, text) {
+  const transporter = nodemailer.createTransport({
+    service: Appconfig.SERVICE,
+    auth: {
+      user: Appconfig.email,
+      pass: Appconfig.password,
+    },
+  });
 
-    const mailoptions = {
-        from,
-        to,
-        subject,
-        text,
-    }
+  const mailoptions = {
+    from,
+    to,
+    subject,
+    text,
+  }
 
-return new Promise((resolve, reject)=>{
-    transporter.sendMail(mailoptions, (error, info)=>{
-        if(error){
-            reject(error);
-        }
-        else{
-            resolve(info);
-        }
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailoptions, (error, info) => {
+      if (error) {
+        reject(error);
+      }
+      else {
+        resolve(info);
+      }
     })
-})
+  })
 }
 
 function response(res, data, message, status, error) {
-    const responsedata = {
-        status,
-        message,
-        data: data,
-        error: error || null,
-    };
-    res.status(status);
-    res.format({
-        json: () => {
-            res.json(responsedata)
-        }
-    })
+  const responsedata = {
+    status,
+    message,
+    data: data,
+    error: error || null,
+  };
+  res.status(status);
+  res.format({
+    json: () => {
+      res.json(responsedata)
+    }
+  })
 }
 
 module.exports = {
-    sendMail,
-    response
+  sendMail,
+  response
 };
